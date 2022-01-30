b35b446d5c4478c395758aad79c6c5f253771f0a
Syahrizal Hanif <syahrizalhanif@gmail.com>
2022-01-30 21:11:04 +0700

    Print dan Komentar

--- hello.dart
+++ hello.dart
@@ -1,5 +1,8 @@
 //dart hello world
 void main() {
+  //different between print with $ and with ${} itu kalo $ semua yang di print
+  /* Sedangkan kalo pake ${} bisa cetak value didalam array */
+
   var name = 'Voyager I';
   var year = 1977;
   var antennaDiameter = 3.7;
@@ -12,6 +15,6 @@ void main() {
   print('Name : $name');
   print(year);
   print(antennaDiameter);
-  print(flybyObjects);
+  print('Planet Pertama : ${flybyObjects[0]}');
   print(image);
 }
