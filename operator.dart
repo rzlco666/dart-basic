111146ed9f9bf7a834b3a70354284c8dbcc2abca
Syahrizal Hanif <syahrizalhanif@gmail.com>
2022-02-13 16:19:34 +0700

    Arithmetic Operators

---
+++ operator.dart
@@ -0,0 +1,30 @@
+void main(){
+  var a = 4;
+  var b = 5;
+
+  //Operand -> Representasi data (a & b)
+  //Operator -> Representasi perintah (+)
+
+  //Arithmetic Operators
+  //Penjumlahan (+)
+  var penjumlahan = a + b;
+  print(penjumlahan);
+  //Pengurangan (-)
+  var pengurangan = a - b;
+  print(pengurangan);
+  //Perkalian (*)
+  var perkalian = a * b;
+  print(perkalian);
+  //Pembagian (/)
+  var pembagian = a / b;
+  print(pembagian);
+  //Modulus (%)
+  var modulus = a % b;
+  print(modulus);
+
+  //Equality Operators
+
+  //Logical Operators
+
+  //Relational Operators
+}
