d66971cb1e184b956b2a1d5d2bbd594ced395f56
Syahrizal Hanif <syahrizalhanif@gmail.com>
2022-02-14 19:29:46 +0700

    Create formula

---
+++ test2.dart
@@ -0,0 +1,19 @@
+void main(){
+  //rumus menghitung keliling persegi
+  int sisi = 10;
+  int keliling = sisi * 4;
+  print("Keliling persegi adalah $keliling");
+
+  //rumus menghitung luas lingkaran
+  double phi = 3.14;
+  double jari = 10;
+  double luas = phi * jari * jari;
+  print("Luas lingkaran adalah $luas");
+
+  //rumus menghitung volume balok
+  int panjang = 6;
+  int lebar = 8;
+  int tinggi = 10;
+  int volume = panjang * lebar * tinggi;
+  print("Volume balok adalah $volume");
+}
