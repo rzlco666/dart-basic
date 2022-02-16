721d7b82ec6838dc3d27d29ab59a2fc6816f016f
Syahrizal Hanif <syahrizalhanif@gmail.com>
2022-02-16 20:44:52 +0700

    Create if else statement

---
+++ decision.dart
@@ -0,0 +1,20 @@
+void main(){
+
+  int a = 3;
+  if(a == 0){
+    print("a is zero");
+  }else{
+    if(a % 2 == 0){
+      print("a is even");
+    }else{
+      print("a is odd");
+    }
+  }
+
+  String name = "Ronaldo";
+  if(name == "Ronaldo"){
+    print("Ronaldo");
+  }else {
+    print("Not Ronaldo");
+  }
+}
