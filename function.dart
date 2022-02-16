01d94cc49863f10196ea9373291f770fbeae8f3b
Syahrizal Hanif <syahrizalhanif@gmail.com>
2022-02-16 20:09:41 +0700

    Cubes formula function

--- function.dart
+++ function.dart
@@ -2,9 +2,17 @@ void main() {
   
   String name = 'Cristiano Ronaldo';
   intro(name);
+
+  int side = 10;
+  print(cubes(side));
 }
 
 //create a intro function
 void intro(String name) {
   print('Hola como estas $name');
+}
+
+//function cubes formula
+int cubes(int side) {
+  return side * side * side;
 }
